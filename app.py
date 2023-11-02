e6865fd811849fed8169cbd6d51bcac3279d6783
bruceyapo <bruceyapo2000@gmail.com>
2023-11-02 23:15:29 +0000

    ajout de requete de modification incomplet 1

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, request,  redirect, url_for
 import pyodbc  as odbccon
-conn = odbccon.connect("Driver={ODBC Driver 17 for SQL Server};"
-                      "Server=DESKTOP-QQGKONI\SQLEXPRESS;"
-                      "Database=e_magasin;"
-                      "Trusted_Connection=yes;")
+# from sqlalchemy import create_engine, Session, String
+# from flask_sqlalchemy import SQLAlchemy
 
-cursor = conn.cursor()
 # cursor.execute('SELECT * FROM Table')
 
 # for row in cursor:
 #     print('row = %r' % (row,))
 
 app = Flask(__name__)
+conn = odbccon.connect("Driver={ODBC Driver 17 for SQL Server};"
+                      "Server=DESKTOP-QQGKONI\SQLEXPRESS;"
+                      "Database=e_magasin;"
+                      "Trusted_Connection=yes;")
 
+cursor = conn.cursor()
 ## Magasin
 @app.route("/")
 def Connexion():
@@ -56,7 +58,7 @@ def Succes_supp():
 @app.route("/List_produit/")
 def Produit():
     # Exécution de la requête de sélection
-    cursor = conn.cursor()
+    # cursor = conn.cursor()
     cursor.execute( "SELECT Id, Nom_produit, Categorie, PrixUnitaire, Description FROM Produit ")
     list = cursor.fetchall()
     # Commit des modifications
@@ -67,31 +69,10 @@ def Produit():
 @app.route("/Ajout_produit/", methods=['GET'])
 def Ajout_produit():
     return render_template("Ajout_produit.html")
-# --------------------------------
-# @app.route('/Ajouter/')
-# def Ajouter():
-#     return render_template("Ajouter.html")
-
-# @app.route('/submit_add/', methods=['POST'])
-# def submit_add():
-#     Nom = request.form['Nom_magasin']
-#     Adresse = request.form['Adresse_magasin']
-#     Telephone = request.form['Telephone']
-#     email = request.form['mail']
-
-#     cursor = conn.cursor()
-#     cursor.execute(
-#             "INSERT INTO Magasin (Nom_magasin, Addresse_magasin, Telephone, E_mail) VALUES (?, ?, ?, ?)",
-#             (Nom, Adresse, Telephone, email)
-#         )
-#     conn.commit()
-#     cursor.close()
-#     return render_template("Magasin.html")
-# ---------------------------------
 #Route pour traiter le formulaire d'ajout
 @app.route("/Succes_ajout_produit/", methods=['POST'])
 def Succes_ajout_produit():
-      
+    cursor = conn.cursor()
     # Récupération des données du formulaire
     Nom_produit = request.form["Nom_produit"]
     Categorie = request.form["Categorie"]
@@ -106,47 +87,54 @@ def Succes_ajout_produit():
         "Description": Description
     }
     # Exécution de la requête d'insertion
-    cursor = conn.cursor()
+    # cursor = conn.cursor()
     cursor.execute(
         f"INSERT INTO Produit (Nom_produit, Categorie, PrixUnitaire,Description) VALUES ('{list['Nom_produit']}', '{list['Categorie']}', '{list['PrixUnitaire']}', '{list['Description']}')"
     )
-    cursor = conn.cursor()
-    cursor.execute( "SELECT Id, Nom_produit, Categorie, PrixUnitaire, Description FROM Produit ")
-    listaj = cursor.fetchall()
-    # Commit des modifications
-    conn.commit()
-    # conn.close()
-    return render_template("Succes_ajout_produit.html", list=list ,listaj=listaj)
-
-#Route pour traiter le formulaire de Modification
-@app.route("/Modifier_produit/<int:_id>")
-def Modifier_produit(_id):
-    cursor = conn.cursor()
+    # cursor = conn.cursor()
     cursor.execute( "SELECT Id, Nom_produit, Categorie, PrixUnitaire, Description FROM Produit ")
     list = cursor.fetchall()
     # Commit des modifications
     conn.commit()
-    return render_template("Modifier_produit.html", list=list )
+    # conn.close()
+    return render_template("Succes_ajout_produit.html", list=list)
 
-@app.route("/Succes_modif_prod/", methods=["POST"])
-def Succes_modif_prod():
-    cursor = conn.cursor()
+#Route pour traiter le formulaire de Modification
+@app.route("/Modifier_produit/<int:_id>" , methods=['GET'])
+def Modifier_produit(_id):
+    # cursor = conn.cursor()
+    # cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM Produit WHERE Id = ?", _id)
+    
+    list = cursor.fetchall() 
+    conn.commit()  
+    return render_template("Modifier_produit.html", list=list)
+    
+    
+@app.route("/Succes_modif_prod/<int:_id>", methods=['POST'])
+def Succes_modif_prod(_id):
+    # id= int(list.Id)
+    # cursor = conn.cursor()
     Nom_produit = request.form["Nom_produit"]
     Categorie = request.form["Categorie"]
     PrixUnitaire = request.form["PrixUnitaire"]
     Description = request.form["Description"]
-
     # Traitement des données
-    list = {
+    list= {
         "Nom_produit": Nom_produit,
         "Categorie": Categorie,
         "PrixUnitaire": PrixUnitaire,
         "Description": Description
     }
-    cursor.execute(f"UPDATE Produit SET Nom_produit = '{list.Nom_produit}', Categorie = '{list.Categorie}', PrixUnitaire = '{list.PrixUnitaire}', Description = '{list.Description}' ")
+    cursor.execute("UPDATE Produit SET Nom_produit =?, Categorie =?, PrixUnitaire =?, Description =?, WHERE Id=? ", (list.Nom_produit, list.Categorie, list.PrixUnitaire, list.Description, _id))
     conn.commit()
-    cursor.close()
-    return redirect("url_for('Produit')")
+    # cursor.close()
+        # return redirect(url_for('Succes_modif_prod', list=list))
+    cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM Produit WHERE Id = ?",_id)
+    list = cursor.fetchall()
+    conn.commit()
+    return render_template("Succes_modif_prod.html", list=list)
 
 
 @app.route("/Supprimer_produit/")
